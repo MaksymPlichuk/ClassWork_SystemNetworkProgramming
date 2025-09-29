3d7dac78083e45c66ae5c767263d107085bb6cdc
MaskymPlichuk <pilipm449@gmail.com>
2025-09-29 17:54:56 +0300

    Add project files.

---
+++ 01_SPIntroToSystemProgrammingProcesses/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_SPIntroToSystemProgrammingProcesses
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            grid.ItemsSource = Process.GetProcesses();
+            //(grid.SelectedItem as Process)
+            string message = " ";
+            message += "Hellloooooooooo";
+
+            MessageBox.Show(message, "Process info", MessageBoxButton.OK, MessageBoxImage.Information);
+            MessageBox.Show(nameProc.Text);
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            //grid.ItemsSource = Process.GetProcesses();
+            var item = (grid.SelectedItem as Process);
+            item.Kill();
+            string message = $"Task {item.ProcessName} Killed!";
+
+            MessageBox.Show(message, "Process info", MessageBoxButton.OK, MessageBoxImage.Information);
+            //MessageBox.Show(nameProc.Text);
+        }
+    }
+}
