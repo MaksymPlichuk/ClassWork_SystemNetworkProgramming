ec314950fe51fc31860ce81d42ccd1285a0d953d
MaskymPlichuk <pilipm449@gmail.com>
2025-10-15 17:58:01 +0300

    Task 1 to/do

---
+++ 08_NPServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _08_NPServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
