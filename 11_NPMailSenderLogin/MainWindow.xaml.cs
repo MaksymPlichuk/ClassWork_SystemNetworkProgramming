a659f84e95573049292c8dd0aa42ec3936ce5b0e
MaskymPlichuk <pilipm449@gmail.com>
2025-10-22 17:44:03 +0300

    Task 1 to/do

---
+++ 11_NPMailSenderLogin/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _11_NPMailSenderLogin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoginWindow window = new LoginWindow(Login,Password);
+            if (window.ShowDialog() == true)
+            {
+                MessageBox.Show(Login + " " + Password);
+            }
+            window.Hide();
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
