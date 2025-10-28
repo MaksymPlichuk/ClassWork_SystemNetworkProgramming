706b3d8c3153fa0922c5502fd6f2d71bb8e04bd7
Maksym <pilipm449@gmail.com>
2025-10-28 23:45:51 +0200

    Task 1 done HW

--- 10_NPCarRegionClientUI/MainWindow.xaml.cs
+++ 10_NPCarRegionClientUI/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +21,104 @@ namespace _10_NPCarRegionClientUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        NetworkStream ns = null;
+        StreamReader streamReader = null;
+        StreamWriter streamWriter = null;
+        CancellationTokenSource cancellationToken;
+
+        IPEndPoint serverEndPoint;
+        TcpClient client;
+        int serverPort = int.Parse(ConfigurationManager.AppSettings["ServerPort"]!);
+        string serverIp = ConfigurationManager.AppSettings["ServerAdress"]!;
+
+
+        MessageInfo minfo;
+        static ObservableCollection<MessageInfo> messages = new ObservableCollection<MessageInfo>();
         public MainWindow()
         {
             InitializeComponent();
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+            client = new TcpClient();
+            minfo = new MessageInfo();
+
+            this.DataContext = messages;
+            this.ipField.Text = serverIp;
+            this.portField.Text = serverPort.ToString();
+        }
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                client.Connect(serverEndPoint);
+                ns = client.GetStream();
+                streamReader = new StreamReader(ns);
+                streamWriter = new StreamWriter(ns);
+
+                cancellationToken = new CancellationTokenSource();
+                Listen(cancellationToken.Token);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string msg = sendTxt.Text;
+            if (string.IsNullOrWhiteSpace(msg)) return;
+
+            streamWriter.WriteLine(msg);
+            streamWriter.Flush();
+
+            //MessageBox.Show(messages.Count.ToString());
+        }
+        private async void Listen(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var res = await streamReader.ReadLineAsync();
+
+                if (minfo.Username == null) { minfo.Username = client.Client.LocalEndPoint.ToString(); }
+                messages.Add(new MessageInfo(res, DateTime.Now, minfo.Username));
+            }
+        }
+        public class MessageInfo
+        {
+            public string Message { get; set; }
+            public DateTime Time { get; set; }
+            public string Username { get; set; }
+
+            public MessageInfo()
+            {
+
+            }
+            public MessageInfo(string m, DateTime t, string username)
+            {
+                Message = m; Time = t;
+                Username = username;
+            }
+
+            public override string ToString()
+            {
+                return $"User: {Username}. Message: {Message}. Time: {Time}";
+            }
+        }
+
+        private void UsernameButton_Click(object sender, RoutedEventArgs e)
+        {
+            minfo.Username = userTxt.Text;
+        }
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            cancellationToken?.Cancel();
+            streamReader?.Close();
+            streamWriter?.Close();
+            ns?.Close();
+            client?.Close();
+            MessageBox.Show("Disconected Succesfully");
         }
     }
 }
--- 10_NPCarRegionServer/Program.cs
+++ 10_NPCarRegionServer/Program.cs
@@ -1,10 +1,97 @@
-﻿namespace _10_NPCarRegionServer
+﻿using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _10_NPCarRegionServer
 {
     internal class Program
     {
+        class Server
+        {
+
+            string ip = "127.0.0.1";
+            int port = 4040;
+
+            TcpListener server_listener;
+
+            Dictionary<string,string>carRegions = new Dictionary<string,string>();
+            public Server()
+            {
+                server_listener = new TcpListener(new IPEndPoint(IPAddress.Parse(ip), port));
+                Console.WriteLine("Server started on port: 4040");
+                carRegions = new()
+                {
+                    {"Автономна Республіка Крим", "АК, КК"},
+                    {"Вінницька", "АВ, КВ"},
+                    {"Волинська", "АС, КС"},
+                    {"Дніпропетровська", "АЕ, КЕ"},
+                    {"Донецька", "АН, КН"},
+                    {"Житомирська", "АМ, КМ"},
+                    {"Закарпатська", "АО, КО"},
+                    {"Запорізька", "АР, КР"},
+                    {"Івано-Франківська", "АТ, КТ"},
+                    {"Київ", "АА, КА"},
+                    {"Київська", "АІ, КІ"},
+                    {"Кіровоградська", "ВА, НА"},
+                    {"Луганська", "ВВ, НВ"},
+                    {"Львівська", "ВС, НС"},
+                    {"Миколаївська", "ВЕ, НЕ"},
+                    {"Одеська", "ВН, НН"},
+                    {"Полтавська", "ВІ, НІ"},
+                    {"Рівненська", "ВК, НК"},
+                    {"Сумська", "ВМ, НМ"},
+                    {"Севастопіль", "СН, ІН"},
+                    {"Тернопільська", "ВО, НО"},
+                    {"Харківська", "АХ, КХ"},
+                    {"Херсонська", "ВТ, НТ"},
+                    {"Хмельницька", "ВХ, НХ"},
+                    {"Черкаська", "СА, ІА"},
+                    {"Чернівецька", "СЕ, ІЕ"},
+                    {"Чернігівська", "СВ, ІВ"}
+                };
+            }
+
+            public void Start()
+            {
+                string reply;
+                server_listener.Start();
+                Console.WriteLine("Waiting for Connection....");
+                TcpClient client = server_listener.AcceptTcpClient();
+                Console.WriteLine($"Connection established! with [{client.Client.LocalEndPoint}]");
+
+                NetworkStream ns = client.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+
+                while (true) { 
+
+                    string msg = sr.ReadLine()!;
+                    Console.WriteLine($"Member: [{client.Client.LocalEndPoint}]  Message: [{msg}]");
+
+
+                    if (carRegions.ContainsKey(msg))
+                    {
+                        reply = carRegions[msg];
+                    }
+                    else
+                    {
+                        var region = carRegions.FirstOrDefault(x => x.Value.Contains(msg)).Key;
+
+                        if (region != null) { reply = region; }
+                        else { reply = "Wrong Code/Region"; }
+                    }
+
+                    sw.WriteLine( reply );
+                    sw.Flush();
+                }
+
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Server s = new Server();
+            s.Start();
         }
     }
 }
